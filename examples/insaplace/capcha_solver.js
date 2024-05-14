bf1110251bd0d8bf57c3199b2499120cbba6e25f
Mubelotix <mubelotix@gmail.com>
2024-05-14 19:35:40 +0200

    Improve capcha solver

--- examples/insaplace/capcha_solver.js
+++ examples/insaplace/capcha_solver.js
@@ -1,4 +1,10 @@
 console.log("Solve capcha")
-let resp = await fetch("https://insagenda.fr/queue-capcha");
-let body_text = await resp.text();
-await this.placePixel(body_text);
+this.timerElement.textContent = "Veuillez patienter...";
+try {
+    let resp = await fetch("https://insagenda.fr/queue-capcha");
+    let body_text = await resp.text();
+    this.timerElement.textContent = "Fait!";
+    await this.placePixel(body_text);
+} catch {
+    this.timerElement.textContent = "Erreur! Connectez-vous sur le site officiel";
+}
