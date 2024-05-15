4e2789bd4dca74bc6ea4b87d578beb77129203ce
Mubelotix <mubelotix@gmail.com>
2024-05-15 03:02:59 +0200

    Add cookie restorer

--- examples/insaplace/main_page.js
+++ examples/insaplace/main_page.js
@@ -5,3 +5,16 @@ let message = {
 window.parent.postMessage(message, "https://insagenda.fr/");
 window.parent.postMessage(message, "https://dev.insagenda.fr/");
 window.parent.postMessage(message, "http://localhost:8088/");
+
+window.addEventListener("message", async (event) => {
+    if (event.origin == "https://insagenda.fr" || event.origin == "https://dev.insagenda.fr" || event.origin == "http://localhost:8088") {
+        if (event.data.ty == "restoreCookies") {
+            console.log("Cookies received");
+            let cookies = event.data.cookies[i];
+            await fetch("/mantalon-override-cookie?name=ip.user_id&value=" + cookies[0]);
+            await fetch("/mantalon-override-cookie?name=ip.user_token&value=" + cookies[1]);
+            await fetch("/mantalon-override-cookie?name=ip.validation_token&value=" + cookies[2]);
+            window.location.reload();
+        }
+    }
+});
