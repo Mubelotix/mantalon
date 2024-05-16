73359938b6f84b768daa34273e8af0cceb08de93
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-05-16 13:03:52 +0200

    session storage everywhere

--- examples/insaplace/main_page.js
+++ examples/insaplace/main_page.js
@@ -9,6 +9,12 @@ window.parent.postMessage(message, "http://localhost:8088/");
 window.addEventListener("message", async (event) => {
     if (event.origin == "https://insagenda.fr" || event.origin == "https://dev.insagenda.fr" || event.origin == "http://localhost:8088") {
         if (event.data.ty == "restoreCookies") {
+            if (window.sessionStorage.getItem("already-restored") == "true") {
+                return;
+            } else {
+                window.sessionStorage.setItem("already-restored", "true");
+            }
+            console.log("Cookies received");
             let cookies = event.data.data;
             await fetch("/mantalon-override-cookie?name=ip.user_id&value=" + cookies[0]);
             await fetch("/mantalon-override-cookie?name=ip.user_token&value=" + cookies[1]);
