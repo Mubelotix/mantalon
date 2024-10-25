ffe66df6561ce098897d3dd22102cd94677e0970
Mubelotix <mubelotix@gmail.com>
2024-10-25 12:23:32 +0200

    Add default header values

--- mantalon-portal/serve-dev.js
+++ mantalon-portal/serve-dev.js
@@ -6,17 +6,18 @@ const server = http.createServer((request, response) => {
     // Internal files
     let internal = false;
     if (request.url == "/mantalon/mantalon_client.js") {
-        request.url = "/node_modules/mantalon-client/mantalon_client.js";
+        request.url = "/mantalon-client/pkg/mantalon_client.js";
         internal = true;
     } else if (request.url == "/mantalon/mantalon_client_bg.wasm") {
-        request.url = "/node_modules/mantalon-client/mantalon_client_bg.wasm";
+        request.url = "/mantalon-client/pkg/mantalon_client_bg.wasm";
         internal = true;
     } else if (request.url == "/sw_bundle.js") {
+        request.url = "/mantalon-portal/sw_bundle.js";
         internal = true;
     }
     if (internal) {
         return handler(request, response, {
-            "public": "./",
+            "public": "..",
         });
     }
 
--- mantalon-portal/src/sw.ts
+++ mantalon-portal/src/sw.ts
@@ -19,6 +19,140 @@ var initError = null;
 var manifest: Manifest;
 var globalProxiedFetch: ProxiedFetchType;
 
+function addDefaultHeaders(headers: Headers, destination: RequestDestination, mode: RequestMode): Headers {
+    if (!headers.has("accept")) {
+        let acceptValue: string;
+        switch (destination) {
+            case "": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "audio": {
+                acceptValue = "audio/*, */*;q=0.1";
+                break;
+            }
+            case "audioworklet": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "document": {
+                acceptValue = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8";
+                break;
+            }
+            case "embed": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "font": {
+                acceptValue = "application/font-woff2;q=1.0,application/font-woff;q=0.9,*/*;q=0.8";
+                break;
+            }
+            case "frame":
+            case "iframe": {
+                acceptValue = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                break;
+            }
+            case "image": {
+                acceptValue = "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8";
+                break;
+            }
+            case "manifest": {
+                acceptValue = "application/manifest+json,*/*;q=0.8";
+                break;
+            }
+            case "object": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "paintworklet": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "report": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "script": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "sharedworker": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "style": {
+                acceptValue = "text/css,*/*;q=0.1";
+                break;
+            }
+            case "track": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "video": {
+                acceptValue = "video/*, */*;q=0.1";
+                break;
+            }
+            case "worker": {
+                acceptValue = "*/*";
+                break;
+            }
+            case "xslt": {
+                acceptValue = "*/*";
+                break;
+            }
+            default: {
+                acceptValue = "*/*";
+                break;
+            }
+        }
+        headers.set("accept", acceptValue);
+    }
+    // TODO: Add accept-encoding header
+    if (!headers.has("accept-language")) {
+        headers.set("accept-language", "en-US,en;q=0.9");
+    }
+    // TODO: Add cache and pragma header defaults
+    if (!headers.has("sec-ch-ua")) {
+        headers.set("sec-ch-ua", '"Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"');
+    }
+    if (!headers.has("sec-ch-ua-mobile")) {
+        headers.set("sec-ch-ua-mobile", "?0");
+    }
+    if (!headers.has("sec-ch-ua-platform")) {
+        headers.set("sec-ch-ua-platform", '"Linux"');
+    }
+    if (!headers.has("sec-fetch-dest")) {
+        headers.set("sec-fetch-dest", destination);
+    }
+    if (!headers.has("sec-fetch-mode")) {
+        if (mode === "navigate") {
+            headers.set("sec-fetch-mode", "navigate");
+        } else {
+            headers.set("sec-fetch-mode", "no-cors");
+        }
+    }
+    if (!headers.has("sec-fetch-site")) {
+        if (mode === "navigate") {
+            headers.set("sec-fetch-site", "none");
+        } else {
+            headers.set("sec-fetch-site", "same-origin");
+        }
+    }
+    if (!headers.has("sec-fetch-user") && mode === "navigate") {
+        headers.set("sec-fetch-user", "?1");
+    }
+    if (!headers.has("sec-gpc")) {
+        headers.set("sec-gpc", "1");
+    }
+    if (!headers.has("upgrade-insecure-requests")) {
+        headers.set("upgrade-insecure-requests", "1");
+    }
+    if (!headers.has("user-agent")) {
+        headers.set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36");
+    }
+    return headers;
+}
+
 async function proxy(event: FetchEvent): Promise<Response> {
     // Get the actual URL of the request
     let url = new URL(event.request.url);
@@ -89,9 +223,13 @@ async function proxy(event: FetchEvent): Promise<Response> {
         return await fetch(event.request);
     }
 
+    // Edit headers
+    let requestHeaders = new Headers(event.request.headers);
+    requestHeaders = addDefaultHeaders(requestHeaders, event.request.destination, event.request.mode);
+
     let initialResponse = await globalProxiedFetch(url, {
         method: event.request.method,
-        headers: event.request.headers,
+        headers: requestHeaders,
         body: event.request.body,
     });
 
